d38818c2f3d38404872aedca4278fa4deee8525e
Petr Safarik <petos.safarik@gmail.com>
2025-09-09 18:31:04 +0200

    Add config flow, still broken tho

---
+++ config_flow.py
@@ -0,0 +1,22 @@
+# config_flow.py
+from homeassistant import config_entries
+from .const import DOMAIN
+import voluptuous as vol
+
+DATA_SCHEMA = vol.Schema({
+    vol.Required("username"): str,
+    vol.Required("password"): str,
+    vol.Required("client_id"): str,
+    vol.Required("client_secret"): str,
+    vol.Required("date_from"): str,
+    vol.Required("date_to"): str,
+})
+
+class HaVodarenskaFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="Vodárenská (VAS API)", data=user_input)
+
+        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
